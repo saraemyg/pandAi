449ffc2baa49c8203b960ebe94cface6483c69b6
NisaaaNabilah <nisanabilah1010@gmail.com>
2025-04-05 21:04:25 +0800

    test

--- website/__init__.py
+++ website/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'saranadiafiza'
+    app.config['SECRET_KEY'] = 'saranadianisa'
 
     @app.route('/')
     def home():
